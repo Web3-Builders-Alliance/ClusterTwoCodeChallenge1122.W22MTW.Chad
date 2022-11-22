3d35cf51d8b17cd01746a6b03448dbef134ee2d5
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-11-22 15:06:13 -0500

    fix build script

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 set -o pipefail
 
 docker run --rm -v "$(pwd)":/code \
-  --mount type=volume,source="$(basename "$projectPath")_cache",target=/code/target \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   cosmwasm/workspace-optimizer:0.12.5
